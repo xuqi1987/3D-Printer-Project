a9fdc608fd2cd164d628e21e71980fd460c7f33a
XuQi <xuqi.jx@gmail.com>
2024-08-14 22:52:14 +0800

    add lvgl

--- Pico_LCD_st7789/src/main.cpp
+++ Pico_LCD_st7789/src/main.cpp
@@ -1,38 +1,67 @@
-/*
-  This sketch demonstrates the use of the horizontal and vertical gradient
-  rectangle fill functions.
+#include <lvgl.h>
 
-  Example for library:
-  https://github.com/Bodmer/TFT_eSPI
+#include <TFT_eSPI.h>
 
-  Created by Bodmer 27/1/22
-*/
+#define TFT_HOR_RES   240
+#define TFT_VER_RES   240
 
-#include <TFT_eSPI.h>       // Include the graphics library
-TFT_eSPI tft = TFT_eSPI();  // Create object "tft"
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-// -------------------------------------------------------------------------
-// Setup
-// -------------------------------------------------------------------------
-void setup(void) {
-  tft.init();
-  tft.setRotation(1);
-  tft.fillScreen(TFT_DARKGREY);
-  tft.setTextFont(2);
+
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+    /*For example  ("my_..." functions needs to be implemented by you)
+    int32_t x, y;
+    bool touched = my_get_touch( &x, &y );
+
+    if(!touched) {
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else {
+        data->state = LV_INDEV_STATE_PRESSED;
+
+        data->point.x = x;
+        data->point.y = y;
+    }
+     */
+}
+void setup()
+{
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.begin( 9600 );
+    Serial.println( LVGL_Arduino );
+
+    lv_init();
+
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    lv_tick_set_cb(millis);
+
+    /* register print function for debugging */
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print );
+#endif
+
+    lv_display_t * disp;
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    lv_obj_t *label = lv_label_create( lv_scr_act() );
+    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+    Serial.println( "Setup done" );
 }
 
-// -------------------------------------------------------------------------
-// Main loop
-// -------------------------------------------------------------------------
 void loop()
 {
-  tft.fillRectHGradient(0, 0, 160, 50, TFT_MAGENTA, TFT_BLUE);
-  tft.setCursor(10,10);
-  tft.print("Horizontal gradient");
-
-  tft.fillRectVGradient(0, 60, 160, 50, TFT_ORANGE, TFT_RED);
-  tft.setCursor(10,70);
-  tft.print("Vertical gradient");
-
-  while(1) delay(100); // Wait here
+    lv_task_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
 }
